7c8fd9b12e33d7b1a5678830c8abcab08d9dfe23
Maxime Mardino DASSI <136202839+Mardino229@users.noreply.github.com>
2025-04-11 17:10:00 +0200

    Update AdminProfiles.tsx

--- src/pages/AdminPages/AdminProfiles.tsx
+++ src/pages/AdminPages/AdminProfiles.tsx
@@ -109,7 +109,7 @@ export default function AdminProfiles() {
             <PageBreadcrumb pageTitle="Paramètres" pagePath="/admin/home" />
             <div className="rounded-2xl border border-gray-200 bg-white p-2 dark:border-gray-800 dark:bg-white/[0.03] lg:p-6">
                 <h3 className="mb-5 text-lg font-semibold text-gray-800 dark:text-white/90 lg:mb-7">
-                    Profile
+                    Profil
                 </h3>
                 <div className="space-y-6">
                     <AdminMetaCard user={user} />
