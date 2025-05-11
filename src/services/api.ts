303494d4505491e099321065df70fd6341688131
Maxime Mardino DASSI <136202839+Mardino229@users.noreply.github.com>
2025-05-11 18:46:21 +0100

    Update api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // const apiUrl = 'http://127.0.0.1:8000/api'
-const apiUrl = ' https://renal-track-454a57ef8198.herokuapp.com/api'
+const apiUrl = 'https://renalcare.nidas.me/api'
 
 export const axiosClient = axios.create({
     baseURL: apiUrl,
